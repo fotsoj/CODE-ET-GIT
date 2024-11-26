bf106844fccdee570eec5d1688db4f2bdf98e924
Junior FOTSO - 3il <fotsoj@3il.fr>
2024-11-26 17:58:15 +0100

    correction de l'exercice 2

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,20 +1,55 @@
 public class GestionnaireNotes {
 
-    // Affiche note et moyennes
+    /**
+     * Affiche note et moyennes
+     */
     public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
-        // Affichage des notes
+
+        /**
+         * Affichage des notes
+         */
+        afficherNote(nomEtudiant,note);
+        
+
+        /**
+         * Calcul de la somme des notes
+         */
+        sommeNote(note);
+
+        /**
+         * Affichage de la moyenne
+         */
+        afficheMoyenne(moyenne);
+    }
+
+    /**
+     * Methode pour afficher la note 
+     * @param nomEtudiant qui donne le nom de l'étudiant
+     * @param note qui donne la note de l'étudiant 
+     */
+    public static void afficherNote(String nomEtudiant, int note){
         System.out.println("Notes de " + nomEtudiant + ":");
         for (int note : notes) {
             System.out.println("- " + note);
         }
+    }
 
-        // Calcul de la somme des notes
+    /**
+     * Methode pour calculer la somme
+     * @param note qui donne la note de l'étudiant 
+     */
+    public static void sommeNote(int note){
         int somme = 0;
         for (int note : notes) {
             somme += note;
         }
+    }
 
-        // Affichage de la moyenne
+    /**
+     * Methode pour afficher la moyenne
+     * @param somme qui donne la somme totale de toutes les notes
+     */
+    public static void afficheMoyenne(int somme){
         double moyenne = (double) somme / notes.length;
         System.out.println("Moyenne : " + moyenne);
     }
